d582fba1e6e4e248e03971ad0f46f356d5d06b0d
Andrew Jackson <inactionjackson@gmail.com>
2019-07-05 18:58:03 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+
+const parseRedditPosts = (data) =>{
+    try{
+    const posts = data.data.children;
+    posts.splice(0,1);
+    return posts;
+    } catch(e){
+        console.error(e);
+    }
+    
+}
+
+
+app.get('/api/redditviewer/:sub',(req,res)=>{
+    axios.get("http://reddit.com/r/"+req.params.sub+".json")
+        .then(response =>{
+            try{
+            res.json(parseRedditPosts(response.data));
+            }catch(e){
+                console.error(e);
+            }
+        })
+    
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server listening on port ${PORT}`);
+})
