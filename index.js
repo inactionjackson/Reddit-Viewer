99ed0dac5cf8741875ddb8f3d3f0968cf17c1375
Andrew Jackson <inactionjackson@gmail.com>
2019-07-15 14:52:01 -0400

    updated build path to work with server

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const parseRedditPosts = (data) =>{
     
 }
 
+app.use(express.static('public'));
 
 app.get('/api/redditviewer/:sub',(req,res)=>{
     axios.get("http://reddit.com/r/"+req.params.sub+".json")
