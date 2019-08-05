6a029a74b8088465d3b8879c2042272e154b7234
Andrew Jackson <inactionjackson@gmail.com>
2019-08-05 17:40:32 -0400

    fixed build issue with client before merge

--- index.js
+++ index.js
@@ -1,46 +1,56 @@
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
-
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.REACT_APP_SERVER_PORT || 5000;
+const path = require("path");
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, "client", "build")));
 
-const parseRedditPosts = (data) =>{
-    try{
+const parseRedditPosts = data => {
+  try {
     const posts = data.data.children;
-    posts.splice(0,1);
+    posts.splice(0, 1);
     return posts;
-    } catch(e){
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+app.get("/api/redditviewer/:sub", (req, res) => {
+  axios
+    .get("http://reddit.com/r/" + req.params.sub + ".json")
+    .then(response => {
+      try {
+        res.json(parseRedditPosts(response.data));
+      } catch (e) {
         console.error(e);
-    }
-    
-}
+      }
+    });
+});
+app.get("/api/redditviewer/:sub/:after", (req, res) => {
+  axios
+    .get(
+      "http://reddit.com/r/" +
+        req.params.sub +
+        ".json?after=" +
+        req.params.after
+    )
+    .then(response => {
+      try {
+        res.json(parseRedditPosts(response.data));
+      } catch (e) {
+        console.error(e);
+      }
+    });
+});
 
-app.get('/api/redditviewer/:sub',(req,res)=>{
-    axios.get("http://reddit.com/r/"+req.params.sub+".json")
-        .then(response =>{
-            try{
-            res.json(parseRedditPosts(response.data));
-            }catch(e){
-                console.error(e);
-            }
-        })
-    
-})
-app.get('/api/redditviewer/:sub/:after',(req,res)=>{
-    axios.get("http://reddit.com/r/"+req.params.sub+".json?after="+req.params.after)
-        .then(response =>{
-            try{
-            res.json(parseRedditPosts(response.data));
-            }catch(e){
-                console.error(e);
-            }
-        })
-    
-})
-
-app.listen(PORT, ()=>{
-    console.log(`Server listening on port ${PORT}`);
-})
+const root = path.join(__dirname, "client", "build");
+app.get("/*", (req, res) => {
+  res.sendFile("index.html", { root });
+});
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
